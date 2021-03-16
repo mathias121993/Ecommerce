1ab93cc5bd721037759b2d63eb94ac630a37f3ea
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-03-16 12:02:15 -0300

    Agregado de la función desplegarPregunta() que se activa clickeando el botón/título de cada pregunta

--- js/script.js
+++ js/script.js
@@ -61,3 +61,10 @@ function closePopUp(){
   }
   prevScrollpos = currentScrollPos;
   }
+
+  // Acordeón preguntas frecuentes
+  function desplegarPregunta(y){
+    y.classList.toggle("active");
+    y.nextElementSibling.classList.toggle("mostrar");
+    }
+  
