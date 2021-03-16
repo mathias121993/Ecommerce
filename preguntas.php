53bab69c106c71d956ebfc6cc56e0c194443ea99
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-03-16 12:02:47 -0300

    Inserción de la nueva sección con preguntas

--- preguntas.php
+++ preguntas.php
@@ -6,7 +6,22 @@
 ?>
 
 <main>
-Q&A
+    <section id="faq">
+        <button class="boton-acordeon" onclick="desplegarPregunta(this)">Pregunta 1</button>
+        <div class="respuesta-acordeon">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <button class="boton-acordeon" onclick="desplegarPregunta(this)">Pregunta 2</button>
+        <div class="respuesta-acordeon">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <button class="boton-acordeon" onclick="desplegarPregunta(this)">Pregunta 3</button>
+        <div class="respuesta-acordeon">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </section>
 </main>
 
 <?php
