8497527e2db03a7460363e0273dfeaa901376f1c
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-03-18 19:19:33 -0300

    Actualización de links a css

--- includes/head.php
+++ includes/head.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo TITLE; ?></title>
     <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
+    <!-----Styles-------->
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/producto.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/faq.css">
+    <link rel="stylesheet" href="css/equipo.css">
     <!-----GoogleFonts-------->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,200&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <!-----Bootstrap-------->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/producto.css">
 </head>
 
 <body>
