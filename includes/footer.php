2187e8a46c8ef16bc815e92b2035640862cd5dad
Aylén Bavosa Castro <aylucastro@gmail.com>
2021-03-18 13:49:37 -0300

    Linkeo a los nuevos archivos js

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,10 @@
       <img class="logo-footer" src="./img/oveja.png" alt="Logo">
     </a>
 </footer>
-    <script src="js/script.js"></script>
+    <script src="js/scroll.js"></script>
+    <script src="js/menu-mobile.js"></script>
+    <script src="js/cart-popup.js"></script>
+    <script src="js/faq-acordeon.js"></script>
     <!-------- JS de Bootstrap------->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js"
     integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous">
